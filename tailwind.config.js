dd76330426e3d8087047e84c02b0f1cc849f5c7f
Alexey Yakimov <alex@tumedia.ru>
2021-04-01 18:01:03 +0500

    Add Breadcrumbs block (#96)
    * Add Breadcrumbs block
    * Add Pseudo Elements plugin + Remove Schema.org
    * Add Twig markup
    * Remove Main partial
    * Change Partials directory structure
    * Add Breadcrumbs partials to pages
    * Add generic href attribute a Breadcrumbs item link
    
    Co-authored-by: Pavel Lautsevich <p.lovtsevich@lovata.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const plugin = require('tailwindcss/plugin')
 
 module.exports = {
   corePlugins: {
@@ -17,5 +18,14 @@ module.exports = {
   variants: {},
   plugins: [
     require('@tailwindcss/typography'),
+    require('tailwindcss-pseudo-elements'),
+    plugin(function ({ addUtilities }) {
+      const newUtilities = {
+        '.separator': {
+          content: 'url(/themes/lovata-tailwind-shopaholic/assets/images/separator.svg)',
+        }
+      }
+      addUtilities(newUtilities, ['before'])
+    }),
   ],
 }
