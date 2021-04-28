6dfa28de3e87cd34181795c999e26278f5aef1ba
Alexey Yakimov <alex@tumedia.ru>
2021-04-29 00:34:28 +0500

    Add Error Message block (#152)
    * Add Error Message block
    * Fix Error Message block markup semantic
    
    Co-authored-by: Pavel Lautsevich <p.lovtsevich@lovata.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
   ],
   darkMode: false,
   theme: {
+    fontSize: {
+      error: ['9rem', '1'],
+    },
     extend: {
       gridTemplateColumns: {
         'full': '100%',
@@ -24,7 +27,7 @@ module.exports = {
       },
       backgroundSize: {
         '65': '65%',
-      }
+      },
     }
   },
   variants: {
