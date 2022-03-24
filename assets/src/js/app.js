f7ce108d55e90034842592df26ffda28159050ff
Nik <nik101skryto@gmail.com>
2022-03-24 22:27:09 +0400

    Add code splitting for JS and CSS (#233)
    * Add code splitting for JS
    * Move page template and assets to their folders
    * Fix broken page links

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,21 +1,7 @@
 import '../../../../../modules/system/assets/js/framework';
 import '../../../../../modules/system/assets/js/framework.extras';
-import './validation';
 
-/* * Top Info Block * */
 import '/partials/top-info-block/top-info-block';
-/* * Wish List * */
-import '/partials/product-card/wish-list';
-/* * Product list small * */
+
 import '/partials/product-list-small/product-list-small';
-/* * Account * */
-// TODO: To decide regarding the assets with no markup
-import '/partials/account/sign-up';
-import '/partials/account/sign-in';
-import '/partials/account/restore-password';
-import '/partials/account/account-change-password';
-import '/partials/carousel-main/carousel-main';
-/* * Input Quantity * */
-import '/partials/input-quantity/input-quantity';
-/* * Off Canvas * */
-import '/partials/off-canvas/off-canvas';
+
---
+++ pages/index/index.js
@@ -0,0 +1,7 @@
+import '/partials/product-card/wish-list';
+
+import '/partials/product-list-small/product-list-small';
+
+import '/partials/carousel-main/carousel-main';
+
+console.log('GIGI')
