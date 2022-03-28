0058de346e16ef47ea3b3146d7851ee5504f4315
imnd <imndsu@gmail.com>
2022-03-28 17:00:29 +0700

    Add Product Info block

---
+++ partials/product-item/product-choose.js
@@ -0,0 +1,14 @@
+export default new class ProductChoose {
+  constructor() {
+    let val = 1;
+    $("#counter").html(val);
+    $("#plus-btn").click((event) => {
+      event.preventDefault();
+      $("#counter").html(++val);
+    });
+    $("#minus-btn").click((event) => {
+      event.preventDefault();
+      $("#counter").html(--val);
+    });
+  }
+}();
