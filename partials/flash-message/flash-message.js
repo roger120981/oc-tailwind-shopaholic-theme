18a0d0cb50682bb01b6d26b391a0b8233469b5f4
Nikita Konnov <n.konnov@lovata.com>
2022-05-27 13:08:06 +0400

    Add animation

--- partials/flash-message/flash-message.js
+++ partials/flash-message/flash-message.js
@@ -43,12 +43,16 @@ export default new class Flash {
                 }
 
                 el
-                .addClass(`${color} flash-message fade flex justify-between p-4 rounded-lg`)
+                .addClass(`${color} flash-message duration-500 opacity-0 fade flex justify-between p-4 rounded-lg`)
                 .attr('data-control', null)
                 .on('click', 'button', remove)
                 .append(`<button type="button" class="close p-5 -m-5" aria-hidden="true"><svg width="12" height="12" viewBox="0 0 12 12" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C0.683417 -0.0976311 1.31658 -0.0976311 1.70711 0.292893L6 4.58579L10.2929 0.292893C10.6834 -0.0976311 11.3166 -0.0976311 11.7071 0.292893C12.0976 0.683417 12.0976 1.31658 11.7071 1.70711L7.41421 6L11.7071 10.2929C12.0976 10.6834 12.0976 11.3166 11.7071 11.7071C11.3166 12.0976 10.6834 12.0976 10.2929 11.7071L6 7.41421L1.70711 11.7071C1.31658 12.0976 0.683417 12.0976 0.292893 11.7071C-0.0976311 11.3166 -0.0976311 10.6834 0.292893 10.2929L4.58579 6L0.292893 1.70711C-0.0976311 1.31658 -0.0976311 0.683417 0.292893 0.292893Z" fill="currentColor"/></svg></button>`)
                
                 $('._flash').prepend(el)
+
+                setTimeout(()=>{
+                    el.removeClass('opacity-0')
+                }, 300)
 
                 if(app.nCount > 5){
                     app.obClearAllButton.removeClass('hidden')
@@ -61,13 +65,16 @@ export default new class Flash {
                 var timer = window.setTimeout(remove, 5500)
 
                 function removeElement() {
-                    app.nCount--
-                    el.remove();
-                    let container = $('._flash > .flash-message');
-                    if(container.length <= 5){
-                        app.obClearAllButton.addClass('hidden')
-                        app.nCount = 0;
-                    }
+                    el.addClass('opacity-0')
+                    setTimeout(()=>{
+                        app.nCount--
+                        el.remove();
+                        let container = $('._flash > .flash-message');
+                        if(container.length <= 5){
+                            app.obClearAllButton.addClass('hidden')
+                            app.nCount = 0;
+                        }
+                    },500)
                 }            
 
                 function remove() {
@@ -85,9 +92,12 @@ export default new class Flash {
         })
         this.obClearAllButton.on('click', ()=>{
             let flash = this.obContainer.find('.flash-message');
+            flash.addClass('opacity-0')
             this.obClearAllButton.addClass('hidden');
             this.nCount = 0;
-            flash.remove();
+            setTimeout(()=>{
+                flash.remove();
+            },250)
         })
     }
 }();
