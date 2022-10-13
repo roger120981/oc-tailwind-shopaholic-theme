c961e0a489d8481a66b88d7964d541c4ae030b3b
Nikita Konnov <n.konnov@lovata.com>
2022-10-13 19:46:07 +0400

    jquery rewritten to js in review-list partial

--- partials/review-list/review-list.js
+++ partials/review-list/review-list.js
@@ -1,31 +1,34 @@
+import request from 'oc-request';
+
 export default new class CustomerReviews {
   constructor() {
-    this.obLoadMore = $('._show-more-reviews');
+    this.obLoadMore = document.getElementsByClassName('_show-more-reviews')[0];
     this.obListWrapper = '_review-list';
     
     this.init();
   }
 
   init(){
-    this.obLoadMore.on("click", () => {
-      const iPage = parseInt(this.obLoadMore.attr('data-page'), 10);
+    console.log(this.obLoadMore)
+    this.obLoadMore.addEventListener("click", () => {
+      const iPage = parseInt(this.obLoadMore.dataset.page, 10);
       const iNextPage = iPage + 1;
-      const iMaxPage = parseInt(this.obLoadMore.attr('data-max-page'), 10);
+      const iMaxPage = parseInt(this.obLoadMore.dataset.maxPage, 10);
       
       this.sendAjax(iNextPage);
 
       if (iNextPage >= iMaxPage) {
         this.obLoadMore.remove();
       } else {
-        this.obLoadMore.attr('data-page', iNextPage);
+        this.obLoadMore.setAttribute('data-page', iNextPage);
       }
     });
   }
 
   sendAjax(iNextPage) {
-    $.request('ProductData::onAjaxRequest', {
+    request.sendData('ProductData::onAjaxRequest', {
       data: { page: iNextPage },
-      update: { 'review-list/review-list-ajax': `@.${this.obListWrapper}` }
+      update: {'review-list/review-list-ajax': '@._review-list'}
     });
   }
 }();
