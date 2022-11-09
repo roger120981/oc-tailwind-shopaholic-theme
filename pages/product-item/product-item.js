5a84d1431d53f254f969935530c4077dbae32891
Nikita Konnov <n.konnov@lovata.com>
2022-11-09 10:52:45 +0400

    Fix connections files js

--- pages/product-item/product-item.js
+++ pages/product-item/product-item.js
@@ -1,5 +1,3 @@
-import '/partials/button-wish-list/button-wish-list';
-import '/partials/product-choose/product-choose';
 import '/partials/review-list/review-list';
 import '/partials/product-gallery/product-gallery';
 import '/partials/product-description/product-description';
