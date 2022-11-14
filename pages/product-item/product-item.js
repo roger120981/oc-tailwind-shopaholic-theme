7ce8c59b63a2b398506c1f43f3c19a758394dc1d
Nikita Konnov <n.konnov@lovata.com>
2022-11-14 09:37:42 +0400

    Add Review Add Off-canvas Form

--- pages/product-item/product-item.js
+++ pages/product-item/product-item.js
@@ -2,3 +2,4 @@ import '/partials/product-card/wish-list';
 import '/partials/review-list/review-list';
 import '/partials/product-gallery/product-gallery';
 import '/partials/product-description/product-description';
+import '/partials/product-review-rating/product-review-rating';
