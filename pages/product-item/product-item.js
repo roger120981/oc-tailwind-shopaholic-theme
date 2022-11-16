af7c8232c0d90fc21c04bf67db7b598168597344
Nikita Konnov <n.konnov@lovata.com>
2022-11-16 14:06:42 +0400

    Remove extra import file

--- pages/product-item/product-item.js
+++ pages/product-item/product-item.js
@@ -1,4 +1,3 @@
 import '/partials/review-list/review-list';
 import '/partials/product-gallery/product-gallery';
 import '/partials/product-description/product-description';
-import '/partials/product-review-rating/product-review-rating';
