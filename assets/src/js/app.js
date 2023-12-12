d9da66d8a51aaf2858a3611358547dbc3263180e
Igor <igor-tv@users.noreply.github.com>
2023-12-13 02:49:20 +0300

    Fix broken Top Info block (#301)
    * Fix the inclusion of the top info block
    * Fix top sub menu
    
    ---------
    
    Co-authored-by: Igor Tverdokhleb <i.tverdokhleb@lovata.com>
    Co-authored-by: antalikavasilkova <antalikavasilkova@gmail.com>
    Co-authored-by: Pavel Lautsevich <p.lautsevich@lovata.com>

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -18,6 +18,7 @@ import '/partials/advanced-list/advanced-list';
 import '/partials/product-list/product-list';
 /* * Search Input* */
 import '/partials/search/search';
+import '/partials/top-info-block/top-info-block';
 import '/partials/product-card-checkout/product-card-checkout';
 import '/partials/button-wish-list/button-wish-list';
 import '/partials/product-choose/product-choose';
