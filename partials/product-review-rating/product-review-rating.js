65903b6ff909220d09e671eaf4d6a734d6fc7eaa
Natalliya-kulva <33567896+Natalliya-kulva@users.noreply.github.com>
2023-12-20 14:33:05 +0300

    Fix broken star rating in the review form
    Co-authored-by: antalikavasilkova <antalikavasilkova@gmail.com>

--- partials/product-review-rating/product-review-rating.js
+++ partials/product-review-rating/product-review-rating.js
@@ -37,16 +37,10 @@ export default new class productReviewRating {
     });
   }
 
-  starState(state, count) {
-    if (state) {
-      for (let i = 0; count > i; i++) {
-        this.obContainerStarReview[i].querySelector('path').style.fill = this.sActiveStar;
+  starState(count) {
+      for (let i = 0; i <  this.obContainerStarReview.length; i++) {
+        this.obContainerStarReview[i].querySelector('path').style.fill = this.obContainerStarReview[i].dataset.rating > count ?  this.sDefaultStar : this.sActiveStar;
       }
-    } else {
-      for (let i = 0; count > i; i++) {
-        this.obContainerStarReview[i].querySelector('path').style.fill = this.sDefaultStar;
-      }
-    }
   }
 
   starSelect() {
@@ -54,12 +48,7 @@ export default new class productReviewRating {
     for (let star of this.obContainerStarReview) {
       star.addEventListener('click', (elem) => {
         let active = elem.target.closest('label').dataset.rating;
-        if (active < app.nActive) {
-          app.starState(false, app.nActive);
-          app.starState(true, active);
-        } else {
-          app.starState(true, active);
-        }
+        app.starState(active);
         app.nActive = active;
       });
     }
@@ -69,12 +58,7 @@ export default new class productReviewRating {
     const app = this;
     window.addEventListener('mouseover', (elem) => {
       if (!document.getElementsByClassName('_container-star-review')[0].contains(elem.target)) {
-        if (app.nActive) {
-          app.starState(false, 5);
-          app.starState(true, app.nActive);
-        } else {
-          app.starState(false, 5);
-        }
+        app.starState(app.nActive || 5);
       }
     })
   }
@@ -84,7 +68,7 @@ export default new class productReviewRating {
     for (let star of this.obContainerStarReview) {
       star.addEventListener('mouseover', (elem) => {
         let active = elem.target.closest('label').dataset.rating;
-        app.starState(true, active);
+        app.starState(active);
       });
     }
   }
@@ -120,7 +104,7 @@ export default new class productReviewRating {
       this.obShow.addEventListener('click', () => {
         setTimeout(() => {
           this.obContainerStarReview = document.querySelectorAll('._container-star-review label');
-          this.starState(true, 5);
+          this.starState(5);
           this.nActive = 5;
           this.initEvents();
         }, 10)
