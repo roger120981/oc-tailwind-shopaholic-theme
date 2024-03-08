0e26b641cd9be75c80e4078cd11343a8c48cc2b0
Andrey Kharanenka <kharanenka@gmail.com>
2024-03-08 10:34:45 +0100

    #380 Refactoring tabs partial

--- pages/product-item.js
+++ pages/product-item.js
@@ -1,4 +1,4 @@
-import '/partials/tabs/tabs';
+import '/partials/common/tabs/tabs';
 import '/partials/review-list/review-list';
 import '/partials/product-gallery/product-gallery';
 import '/partials/product-description/product-description';
---
+++ partials/common/tabs/tabs.js
@@ -0,0 +1,10 @@
+import VanillaTabs from '../../assets/src/js/vanila-tabs/vanilla-tabs.js';
+
+oc.pageReady().then(() => {
+  new VanillaTabs({
+    'selector': '._tabs-product',
+    'type': 'horizontal',
+    'responsiveBreak': 840,
+    'activeIndex' : 0
+  });
+});
