016a23090b8c5e78614360e6569551e1c22aaa32
antalikavasilkova <antalikavasilkova@gmail.com>
2024-03-11 14:21:26 +0300

    fix select

---
+++ partials/form/select.js
@@ -0,0 +1,34 @@
+import UrlGeneration from '@oc-shopaholic/url-generation'
+export default class Select {
+  constructor() {}
+  initSummaryEvents() {
+    this.clearOptions();
+  }
+  clearOptions() {
+    document.addEventListener('click', (event) => {
+      const eventNode = event.target;
+      const buttonNode = eventNode.closest('.js-clear-options');
+      if (!buttonNode) {
+        return;
+      }
+      this.clearValueOptions(buttonNode)
+      buttonNode.classList.add('hidden')
+    });
+  }
+
+  clearValueOptions(buttonNode) {
+    const select = buttonNode.parentNode.querySelector('select');
+    select.value = '';
+    select.dispatchEvent(
+      new InputEvent('change', {
+          bubbles: true,
+          cancelable: true,
+        }
+      ));
+  }
+}
+
+oc.pageReady().then(() => {
+   const select = new Select();
+   select.initSummaryEvents();
+});
