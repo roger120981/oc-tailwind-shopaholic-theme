dbd7c88e8a073fa0ea1ac9a3b48a22bdc1d53757
Andrey Kharanenka <kharanenka@gmail.com>
2024-03-12 23:10:17 +0100

    Refactoring tags partial

--- partials/catalog/tags/tags.js
+++ partials/catalog/tags/tags.js
@@ -1,53 +1,27 @@
 import UrlGeneration from '@oc-shopaholic/url-generation'
-import Accordion from '/partials/common/accordion/accordion'
+import {EVENT_OPEN} from '/partials/common/off-canvas/off-canvas'
+import {ProductList} from '/partials/product/product-list/product-list'
+
+const FILTER_TYPE_CHECKBOX = 'checkbox';
+const FILTER_TYPE_SELECT = 'select';
+const FILTER_TYPE_RADIO = 'radio';
+const FILTER_TYPE_BETWEEN = 'between';
+const FILTER_TYPE_PRICE = 'price';
+const FILTER_TYPE_SALE = 'sale';
 
 class TagList {
   constructor () {
     this.mainNode = document.querySelector('._tags');
     this.tagListNode = this.mainNode ? this.mainNode.querySelector('._tags-container') : null;
-    this.resetButtonNode = this.mainNode ? this.mainNode.querySelector('._reset-all-tags') : null;
     this.propertyCount = 0;
     this.tagList = [];
 
     UrlGeneration.init();
-
-    this.obFilter = document.getElementsByClassName('_filter')[0]
-    this.obFilterButton = this.obFilter.querySelectorAll('._show')[0]
-    this.obFilterButtonHide = null
-
-    this.bRepetition = false
-    this.obFilterProperties = []
   }
 
   init () {
-    if (window.innerWidth <= 768) {
-      this.obFilterButton.dispatchEvent(
-        new InputEvent('click', {
-          bubbles: true,
-          cancelable: true,
-        }))
-      this.bRepetition = true
-      this.obFilterButton.setAttribute('data-tags', true)
-      this.obFilterButton.setAttribute('data-show', true)
-      document.getElementsByClassName('_offCanvasContainer')[0].classList.add('hidden')
-
-      this.initTags()
-      this.watchCatalog()
-      this.initFilter()
-    } else {
-
-      this.initTags()
-      this.watchCatalog()
-      this.initFilter()
-    }
-  }
-
-  initTags () {
     this.prepareTagList();
-    this.createTags()
-    this.removeTag()
-    this.removeAllTags()
-    Accordion.make();
+    this.createTags();
   }
 
   prepareTagList() {
@@ -55,10 +29,10 @@ class TagList {
     this.tagList = [];
     for (const [key, value] of UrlGeneration.obSearchParams.entries()) {
       let tagID = null;
-      if (key === 'price') {
-        tagID = 'price';
-      } else if (key === 'sale') {
-        tagID = 'sale';
+      if (key === FILTER_TYPE_PRICE) {
+        tagID = FILTER_TYPE_PRICE;
+      } else if (key === FILTER_TYPE_SALE) {
+        tagID = FILTER_TYPE_SALE;
       } else if (key.indexOf('property[') === 0) {
         this.propertyCount++;
         tagID = key.replace('property[', '').replace(']', '');
@@ -78,18 +52,18 @@ class TagList {
   createTags () {
     this.tagListNode.innerHTML = '';
     if (this.tagList.length === 0) {
+      this.mainNode.classList.add('hidden');
+
       return;
     }
 
-    console.log(this.tagList);
     this.tagList.forEach((tagData) => {
       if (tagData.id === 'price') {
-        // TODO: rewrite for multiple elements
         this.createPriceTag(tagData);
       } else if (tagData.id === 'sale') {
         this.createWithDiscountTag(tagData);
       } else {
-        //this.createPropertyTag(tagData);
+        this.createPropertyTag(tagData);
       }
     });
 
@@ -115,22 +89,18 @@ class TagList {
       return;
     }
 
-    const tagText = this.generatePriceTagText(minValue, maxValue, priceFilterNode);
+    const tagText = this.generateBetweenTagText(minValue, maxValue, priceFilterNode.dataset.translateTo, priceFilterNode.dataset.translateFrom);
     const tagHTML = `${filterName}: ${tagText}`;
 
-    const tagNode = document.createElement('li');
-    tagNode.dataset.name = tagData.id;
-    tagNode.className = 'whitespace-nowrap me-3 mb-3 capitalize bg-white rounded shadow py-3 ps-3 pe-5 flex items-center justify-between text-gray-600 text-base lg:text-sm';
-    tagNode.innerHTML = tagHTML + ` <button class="_delete-tag text-gray-900 p-2 -m-2 ms-3 hover:text-blue-700 active:text-blue-900 focus-visible:ring-1 focus-visible:ring-blue-800 focus-visible:ring-offset-1 focus-visible:ring-offset-white focus:outline-none focus:text-blue-800" type="button" data-id="${tagData.id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg></button>`;
-    this.tagListNode.appendChild(tagNode);
+    this.addTagNode(tagData, tagHTML);
   }
 
-  generatePriceTagText(minValue, maxValue, priceFilterNode) {
+  generateBetweenTagText(minValue, maxValue, minLabel, maxLabel) {
     let tagText = '';
     if (!minValue && maxValue) {
-      tagText = `${priceFilterNode.dataset.translateTo} ${maxValue}`;
+      tagText = `${maxLabel} ${maxValue}`;
     } else if (minValue && (!maxValue || minValue >= maxValue)) {
-      tagText = `${priceFilterNode.dataset.translateFrom} ${minValue}`;
+      tagText = `${minLabel} ${minValue}`;
     } else {
       tagText = `${minValue} - ${maxValue}`;
     }
@@ -147,146 +117,176 @@ class TagList {
 
     const filterName = discountLabelNode.innerText.trim();
 
-    const tagNode = document.createElement('li');
-    tagNode.dataset.name = tagData.id;
-    tagNode.className = 'whitespace-nowrap me-3 mb-3 capitalize bg-white rounded shadow py-3 ps-3 pe-5 flex items-center justify-between text-gray-600 text-base lg:text-sm';
-    tagNode.innerHTML = filterName + ` <button class="_delete-tag text-gray-900 p-2 -m-2 ms-3 hover:text-blue-700 active:text-blue-900 focus-visible:ring-1 focus-visible:ring-blue-800 focus-visible:ring-offset-1 focus-visible:ring-offset-white focus:outline-none focus:text-blue-800" type="button" data-id="${tagData.id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg></button>`;
-    this.tagListNode.appendChild(tagNode);
+    this.addTagNode(tagData, filterName);
   }
 
   createPropertyTag(tagData) {
-    const filterNode = document.querySelector(`_shopaholic-filter-wrapper[data-property-id=${tagData.id}]`);
+    const filterNode = document.querySelector(`._shopaholic-filter-wrapper[data-property-id="${tagData.id}"]`);
     const filterType = filterNode ? filterNode.dataset.filterType : null;
     if (!filterNode || !filterType) {
       return;
     }
 
-    const filterName = this.getFilterLabel(tagData, filterNode, filterType);
-
-    const tagNode = document.createElement('li');
-    tagNode.dataset.name = tagData.id;
-    tagNode.className = 'whitespace-nowrap me-3 mb-3 capitalize bg-white rounded shadow py-3 ps-3 pe-5 flex items-center justify-between text-gray-600 text-base lg:text-sm';
-    tagNode.innerHTML = filterName + ` <button class="_delete-tag text-gray-900 p-2 -m-2 ms-3 hover:text-blue-700 active:text-blue-900 focus-visible:ring-1 focus-visible:ring-blue-800 focus-visible:ring-offset-1 focus-visible:ring-offset-white focus:outline-none focus:text-blue-800" type="button" data-id="${tagData.id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg></button>`;
-    this.tagListNode.appendChild(tagNode);
-  }
-
-  getFilterLabel(tagData, filterNode, filterType) {
-    if (filterType === 'checkbox') {
-
+    if (filterType === FILTER_TYPE_CHECKBOX) {
+      this.createCheckboxFilterTag(tagData, filterNode);
+    } else if (filterType === FILTER_TYPE_SELECT) {
+      this.createSelectFilterTag(tagData, filterNode);
+    } else if (filterType === FILTER_TYPE_RADIO) {
+      this.createRadioFilterTag(tagData, filterNode);
+    } else if (filterType === FILTER_TYPE_BETWEEN) {
+      this.createBetweenFilterTag(tagData, filterNode);
     }
   }
 
-  createTagsFiltres (props) {
-    let id = props.id
-    const elemsDetails = document.getElementById(id)
-    if(!elemsDetails) return false
-    let section =  props.id == 'sale' ?  document.querySelector('label[for="sale"]') : elemsDetails.querySelector('summary')
-    const sectionText = section.innerText.trim()
-    props.value.forEach(prop => {
-      let text = document.querySelector(`[for="${prop}"]`)
-      let tag = document.createElement('li')
-      tag.dataset.name = prop
-      tag.className = 'whitespace-nowrap me-3 mb-3 capitalize bg-white rounded shadow py-3 ps-3 pe-5 flex items-center justify-between text-gray-600 text-base lg:text-sm'
-      tag.innerHTML = `${text ? sectionText + ': ' : sectionText } ${text ? text.innerText : ''}  <button class="_delete-tag text-gray-900 p-2 -m-2 ms-3 hover:text-blue-700 active:text-blue-900 focus-visible:ring-1 focus-visible:ring-blue-800 focus-visible:ring-offset-1 focus-visible:ring-offset-white focus:outline-none focus:text-blue-800" type="button" data-id='${props.id == 'sale' ? 'sale' : prop}'> <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg></button>`
-      this.tagListNode.appendChild(tag)
-    })
-  }
-
-  removeTag () {
-    const tags = document.querySelectorAll('._delete-tag')
-    for (let tag of tags) {
-      tag.addEventListener('click', (elem) => {
-        if (tag.dataset.id == 'price') {
-          // TODO: rewrite for multiple elements
-          const inputs = document.querySelectorAll('#price ._shopaholic-price-filter')
-          inputs.forEach(input => {
-            input.value = ''
-            input.dispatchEvent(
-              new InputEvent('change', {
-                bubbles: true,
-                cancelable: true,
-              }))
-          })
-        } else {
-          const tagElementId =  document.getElementById(`${elem.target.closest('._delete-tag').dataset.id}`)
-          tagElementId.checked = false
-          tagElementId.dispatchEvent(
-            new InputEvent('change', {
-              bubbles: true,
-              cancelable: true,
-            }))
-        }
-        if (this.tagListNode.childElementCount <= 1) {
-          this.mainNode.classList.add('hidden')
-        }
-      })
-    }
-  }
-
-  removeAllTags () {
-    for (let resetAll of document.querySelectorAll('._reset-all-tags')) {
-      resetAll.addEventListener('click', (elem) => {
-        let url = window.location.href.split('?')[0]
-        window.location.href = url
-      })
-    }
-  }
-
-  watchCatalog () {
-    let app = this
-    var target = document.getElementsByClassName('_sorting')[0]
-
-    const config = {
-      childList: true,
-    }
-
-    const callback = function (mutationsList, observer) {
-      for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-          app.initTags()
-          if (app.tagListNode.childElementCount < 1) {
-            app.mainNode.classList.add('hidden')
-          }
-        }
+  createCheckboxFilterTag(tagData, filterNode) {
+    const valueList = tagData.value.split('|');
+    valueList.forEach((value) => {
+      const labelNode = filterNode.querySelector(`label[for="property-${tagData.id}-${value}"]`);
+      if (!labelNode) {
+        return;
       }
-    }
 
-    const observer = new MutationObserver(callback)
+      const filterName = labelNode.innerText.trim();
 
-    observer.observe(target, config)
+      this.addTagNode(tagData, filterName, value);
+    });
   }
 
-  initFilter () {
-    if (!this.obFilterButton) {
+  createSelectFilterTag(tagData, filterNode) {
+    const labelNode = filterNode.querySelector(`option[value="${tagData.value}"]`);
+    if (!labelNode) {
       return;
     }
 
-    this.obFilterButton.addEventListener('click', () => {
-      this.mobile()
-      this.initTags()
-      if (this.bRepetition) {
-        this.obFilterButton.removeAttribute('data-tags')
-        document.getElementsByClassName('_offCanvasContainer')[0].classList.remove('hidden')
-      }
-    })
+    const filterName = labelNode.innerText.trim();
+
+    this.addTagNode(tagData, filterName, tagData.value);
   }
 
-  mobile () {
-    this.mainNode = document.getElementsByClassName('_tags')[0]
-    this.tagListNode = this.mainNode.querySelectorAll('._tags-container')[0]
-    this.resetButtonNode = this.mainNode.querySelectorAll('._reset-all-tags')[0]
-    this.obFilter = document.getElementsByClassName('_filter')[0]
-    this.obFilterButton = this.obFilter.querySelectorAll('._show')[0]
+  createRadioFilterTag(tagData, filterNode) {
+    const labelNode = filterNode.querySelector(`label[for="property-${tagData.id}-${tagData.value}"]`);
+    if (!labelNode) {
+      return;
+    }
 
-    this.propertyCount = null
-    this.obFilterProperties = []
+    const filterName = labelNode.innerText.trim();
+
+    this.addTagNode(tagData, filterName, tagData.value);
+  }
+
+  createBetweenFilterTag(tagData, filterNode) {
+    const filterSummaryNode = filterNode.parentNode.querySelector('summary');
+    const minLabelNode = filterNode.querySelector(`label[for="property-min-${tagData.id}"]`);
+    const maxLabelNode = filterNode.querySelector(`label[for="property-max-${tagData.id}"]`);
+    if (!filterSummaryNode || !minLabelNode || !maxLabelNode) {
+      return;
+    }
+
+    const filterName = filterSummaryNode.innerText.trim();
+    const valueList = tagData.value.split('|');
+    const minValue = valueList[0];
+    const maxValue = minValue.length > 1 ? valueList[1] : null;
+    if (!minValue && !maxValue) {
+      return;
+    }
+
+    const minLabel = minLabelNode.innerText.trim();
+    const maxLabel = maxLabelNode.innerText.trim();
+    const tagText = this.generateBetweenTagText(minValue, maxValue, minLabel, maxLabel);
+    const tagHTML = `${filterName}: ${tagText}`;
+
+    this.addTagNode(tagData, tagHTML, tagData.value);
+  }
+
+  addTagNode(tagData, tagHTML, value) {
+    const tagNode = document.createElement('li');
+    tagNode.dataset.propertyId = tagData.id;
+    tagNode.dataset.propertyValue = value;
+    tagNode.className = 'whitespace-nowrap me-3 mb-3 capitalize bg-white rounded shadow py-3 ps-3 pe-5 flex items-center justify-between text-gray-600 text-base lg:text-sm';
+    tagNode.innerHTML =  `${tagHTML} <button class="_delete-tag text-gray-900 p-2 -m-2 ms-3 hover:text-blue-700 active:text-blue-900 focus-visible:ring-1 focus-visible:ring-blue-800 focus-visible:ring-offset-1 focus-visible:ring-offset-white focus:outline-none focus:text-blue-800" type="button" data-id="${tagData.id}"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg></button>`;
+    this.tagListNode.appendChild(tagNode);
+  }
+
+  static removeTag () {
+    document.addEventListener('click', (event) => {
+      const eventNode = event.target;
+      if (!eventNode.closest('._delete-tag')) {
+        return;
+      }
+
+      const listNode = eventNode.closest('li');
+      const propertyId = listNode ? listNode.dataset.propertyId : null;
+      const propertyValue = listNode ? listNode.dataset.propertyValue : null;
+      if (!propertyId || !propertyValue) {
+        return;
+      }
+
+      let fieldName = `property[${propertyId}]`;
+      if (propertyId === FILTER_TYPE_PRICE) {
+        fieldName = FILTER_TYPE_PRICE;
+      } else if (propertyId === FILTER_TYPE_SALE) {
+        fieldName = FILTER_TYPE_SALE;
+      }
+
+      UrlGeneration.init();
+      let fieldValue = UrlGeneration.obSearchParams.get(fieldName);
+      if (!fieldValue) {
+        return;
+      }
+
+      if (fieldValue === propertyValue || [FILTER_TYPE_PRICE, FILTER_TYPE_SALE].includes(propertyId)) {
+        UrlGeneration.remove(fieldName);
+      } else {
+        fieldValue = fieldValue.replace(propertyValue, '');
+        fieldValue = fieldValue.replace('||', '|');
+
+        UrlGeneration.set(fieldName, fieldValue);
+      }
+
+      UrlGeneration.update();
+      const obProductList = new ProductList();
+      const obListHelper = obProductList.getListHelper();
+      obListHelper.send();
+    });
+  }
+
+  static removeAll () {
+    document.addEventListener('click', (event) => {
+      const eventNode = event.target;
+      const buttonNode = eventNode.closest('._reset-all-tags');
+      if (!buttonNode) {
+        return;
+      }
+
+      UrlGeneration.clear();
+      UrlGeneration.update();
+
+      const mainNode = document.querySelector('._tags');
+      mainNode.classList.add('hidden');
+
+      const obProductList = new ProductList();
+      const obListHelper = obProductList.getListHelper();
+      obListHelper.send();
+    });
   }
 
   static initHandlers() {
-    document.addEventListener('product:list.updated', () => {
+    document.addEventListener('product:filter.updated', () => {
       const obTagList = new TagList();
       obTagList.init();
     });
+
+    document.addEventListener(EVENT_OPEN, (event) => {
+      if (event.detail.id !== 'filter_panel') {
+        return;
+      }
+
+      const obTagList = new TagList();
+      obTagList.init();
+    });
+
+    TagList.removeTag();
+    TagList.removeAll();
   }
 }
 
