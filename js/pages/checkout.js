d626536bb712af131c083ce5309f4c9c59783e9d
Andrey Kharanenka <kharanenka@gmail.com>
2024-04-09 22:15:18 +0200

    Refactoring checkout page

--- js/pages/checkout.js
+++ js/pages/checkout.js
@@ -3,6 +3,5 @@ import '/partials/product/offer-quantity/offer-quantity';
 /* * Checkout * */
 import '/partials/checkout/checkout';
 import '/partials/checkout/coupon';
-import '/partials/checkout/switch';
 import '/partials/checkout/order';
 import '/js/vendor/validation';
