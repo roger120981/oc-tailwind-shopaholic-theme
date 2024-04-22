874cc360226dc3febdd4f3beee861a21a50fdab2
Andrey Kharanenka <kharanenka@gmail.com>
2024-04-22 22:33:23 +0200

    Fix payment method list on checkout page

--- partials/checkout/checkout.js
+++ partials/checkout/checkout.js
@@ -1,15 +1,11 @@
 import ShopaholicCartShippingType from '@oc-shopaholic/shopaholic-cart/shopaholic-cart-shipping-type';
+import ShopaholicCartPaymentMethod from '@oc-shopaholic/shopaholic-cart/shopaholic-cart-payment-method';
 
 class Checkout {
-  constructor() {
-    this.sCash = "._cash";
-    this.sCard = "._card";
-    this.sPaymentCard = "._payment-card";
-  }
-
   init() {
     this.initShippingTypeHandler();
-    this.deliveryTermsHandler();
+    this.initPaymentMethodHandler();
+    this.shippingTypeTermsHandler();
   }
 
   initShippingTypeHandler() {
@@ -17,6 +13,7 @@ class Checkout {
     obShopaholicCartShippingType.setAjaxRequestCallback((obRequestData, inputNode) => {
       obRequestData.update = {
         'checkout/shipping-type-list': '._shipping_type_wrapper',
+        'checkout/payment-method-list': '._payment-method-wrapper',
       }
 
       return obRequestData;
@@ -25,7 +22,21 @@ class Checkout {
     obShopaholicCartShippingType.init();
   }
 
-  deliveryTermsHandler() {
+  initPaymentMethodHandler() {
+    const obShopaholicCartPaymentMethod = new ShopaholicCartPaymentMethod();
+    obShopaholicCartPaymentMethod.setAjaxRequestCallback((obRequestData, inputNode) => {
+      obRequestData.update = {
+        'checkout/shipping-type-list': '._shipping_type_wrapper',
+        'checkout/payment-method-list': '._payment-method-wrapper',
+      }
+
+      return obRequestData;
+    });
+
+    obShopaholicCartPaymentMethod.init();
+  }
+
+  shippingTypeTermsHandler() {
     document.addEventListener('click', (event) => {
       const eventNode = event.target;
       const buttonNode = eventNode.closest('._delivery-terms');
@@ -49,21 +60,6 @@ class Checkout {
         buttonNode.setAttribute('aria-expanded', false);
       }
     });
-  }
-
-  paymentMethod() {
-    document.querySelector('._payment-method').addEventListener('click', (event) => {
-      if (event.target.tagName === 'INPUT') {
-        if (document.querySelector(this.sCash).checked) {
-          document.querySelector(this.sPaymentCard).classList.add('hidden');
-          document.querySelector(this.sPaymentCard).setAttribute('aria-hidden', true);
-        }
-        if (document.querySelector(this.sCard).checked) {
-          document.querySelector(this.sPaymentCard).classList.remove('hidden');
-          document.querySelector(this.sPaymentCard).removeAttribute('aria-hidden');
-        }
-      }
-    })
   }
 }
 
